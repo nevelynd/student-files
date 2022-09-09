39818f251bf5a1e686e2c34bd5581486f0f1f998
nevelynd <evelynho2025@berkeley.edu>
2022-09-09 15:05:23 -0700

    check

--- lab03/randomizedtest/BuggyAList.java
+++ lab03/randomizedtest/BuggyAList.java
@@ -31,7 +31,6 @@ public class BuggyAList<Item> {
             a[i] = items[i];
         }
         items = a;
-        items[size]=null;
     }
 
     /** Inserts X into the back of the list. */
@@ -61,7 +60,7 @@ public class BuggyAList<Item> {
       * returns deleted item. */
     public Item removeLast() {
         if ((size < items.length / 4) && (size > 4)) {
-            resize(size / 4);
+            resize(items.length / 4);
         }
         Item x = getLast();
         items[size - 1] = null;
