0ed9094da8debeefe14af224aa894868a88954af
nevelynd <evelynho2025@berkeley.edu>
2022-09-18 13:52:10 -0700

    prog

--- proj1/deque/ArrayDequeTest.java
+++ proj1/deque/ArrayDequeTest.java
@@ -12,4 +12,25 @@ public class ArrayDequeTest {
         assertTrue("A newly initialized ADeque should be empty", ad1.isEmpty());
         ad1.addFirst("front");
     }
+
+    @Test
+    /* Add large number of elements to deque; check if order is correct. */
+    public void bigLLDequeTest() {
+
+
+        ArrayDeque<Integer> ad1 = new ArrayDeque<Integer>();
+        for (int i = 0; i < 10; i++) {
+            ad1.addLast(i);
+        }
+        for (double i = 0; i < 5; i++) {
+            ad1.removeFirst();
+            //assertEquals("Should have the same value", i, (double) lld1.removeFirst(), 0.0);
+        }
+
+        for (double i = 9; i > 5; i--) {
+            assertEquals("Should have the same value", i, (double) ad1.removeLast(), 0.0);
+        }
+
+
+    }
 }
