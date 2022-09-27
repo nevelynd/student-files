73a96a0b941f54950971212ce1021545bbcffd08
nevelynd <evelynho2025@berkeley.edu>
2022-09-26 17:42:37 -0700

    prog

--- proj1/deque/MaxArrayDequeTest.java
+++ proj1/deque/MaxArrayDequeTest.java
@@ -1,6 +1,6 @@
 package deque;
 import org.junit.Test;
-
+import java.util.Comparator;
 
 
 import static org.junit.Assert.*;
@@ -10,25 +10,41 @@ import static org.junit.Assert.*;
 public class MaxArrayDequeTest<T> {
     @Test
     public void Test1() {
-        //Comparators<T> cc = maxx();
+        ArrayDeque<Integer> ArrayDeque = new ArrayDeque<Integer>();
+        ArrayDeque.addFirst(0);
+        ArrayDeque.addFirst(1);
+        ArrayDeque.addFirst(2);
+        ArrayDeque.addFirst(3);
+        ArrayDeque.addFirst(4);
+        ArrayDeque.addFirst(5);
+        ArrayDeque.addFirst(6);
+        Comparator<Integer> cc = getNameComparator();
         MaxArrayDeque<Integer> ad1 = new MaxArrayDeque<Integer>(null);
 
 
     }
-
-
-    private class Comparatorr<E> {
-        public int maxx(int x, int y) {
-            if (x < y) {
-                return 1;
-            }
-            if (x > y) {
-                return -1;
-            } else {
-                return 0;
-            }
+   
+}
+public static class NameComparator implements Comparator<Integer> {
+    public int compare(int x, int y) {
+        if (x < y) {
+            return 1;
         }
+        if (x > y) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
 
+    public static Comparator<Integer> getNameComparator()  {
+        return new NameComparator();
     }
 
 }
+
+
+
+
+
+  //  }
